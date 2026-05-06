d7e637ac6ddf68f9dc2c30c13919713fc5ddd226
Mehedi Hasan <u1904067@student.cuet.ac.bd>
2026-05-06 20:50:01 +0600

    update access for seller and admin

--- src/module/customer/customer.route.ts
+++ src/module/customer/customer.route.ts
@@ -8,27 +8,28 @@ const router = express.Router();
 
 router.get('/me',auth(Role.CUSTOMER,Role.ADMIN,Role.SELLER),customerController.getMyProfile);
 router.get('/orders',auth(Role.CUSTOMER),customerController.getMyOrder);
-router.get('/orders/:id',auth(Role.CUSTOMER),customerController.getSingleOrder);
+router.get('/orders/:id',auth(Role.CUSTOMER,Role.ADMIN,Role.SELLER),customerController.getSingleOrder);
 
 router.post('/cart',auth(Role.CUSTOMER),customerController.AddItemToCard);
 router.post('/decrement',auth(Role.CUSTOMER),customerController.DecrementCartItem);
 
 router.get('/cart',auth(Role.CUSTOMER),customerController.getMyCartItem);
 router.get('/cart/:id',auth(Role.CUSTOMER),customerController.getMySingleCartItem);
-router.put('/profile',auth(Role.CUSTOMER, Role.SELLER),customerController.editMyProfile);
+router.put('/profile',auth(Role.CUSTOMER,Role.ADMIN ,Role.SELLER),customerController.editMyProfile);
 router.put('/checkout',auth(Role.CUSTOMER),customerController.addShippingAddress);
 router.delete('/:id',auth(Role.CUSTOMER),customerController.deleteCartItem);
 
-router.post('/address',validateRequest(createAddressZodSchema),auth(Role.CUSTOMER),customerController.createAddress);
-router.put('/update-my-address',auth(Role.CUSTOMER,Role.SELLER),customerController.updateAddress);
+router.post('/address',validateRequest(createAddressZodSchema),auth(Role.CUSTOMER,Role.ADMIN,Role.SELLER),customerController.createAddress);
+router.put('/update-my-address',auth(Role.CUSTOMER,Role.SELLER,Role.ADMIN),customerController.updateAddress);
 
 
 router.post('/review',auth(Role.CUSTOMER),validateRequest(createReviewZodSchema),customerController.createReview);
 
 
 router.get('/review',auth(Role.CUSTOMER,Role.ADMIN,Role.SELLER),customerController.getReview);
-router.get('/review/:medicineId',auth(Role.CUSTOMER,Role.ADMIN,Role.SELLER),customerController.getSingleMedicineReview);                    
-router.get('/my-address',auth(Role.CUSTOMER),customerController.getMyAddress);
+router.get('/review/:medicineId',auth(Role.CUSTOMER,Role.ADMIN,Role.SELLER),customerController.getSingleMedicineReview);
+
+router.get('/my-address',auth(Role.CUSTOMER,Role.ADMIN,Role.SELLER),customerController.getMyAddress);
 
 
 
